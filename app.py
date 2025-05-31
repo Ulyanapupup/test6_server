607f27b29d17b02dff14db810785db9968852cac
Ulyanapupup <167175946+Ulyanapupup@users.noreply.github.com>
2025-05-31 14:17:37 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 games = {}  # хранилище активных игр для режима 1.2: {game_id: Game}
 
 # Добавим поле для хранения выбранных ролей в каждой комнате
-# rooms = {
+rooms = {
 #   'ROOMCODE': {
 #       'players': set(session_ids),
 #       'creator': session_id,
